b6c8ff556fca5d4ffa29fba819fefc5e3572025d
Deeka Wong <huangdijia@gmail.com>
2025-11-18 21:06:54 +0800

    Add socket option management to SocketInterface (#12)

--- src/SocketInterface.php
+++ src/SocketInterface.php
@@ -12,8 +12,14 @@ declare(strict_types=1);
 
 namespace Hyperf\Engine\Contract;
 
+use Hyperf\Engine\Contract\Socket\SocketOptionInterface;
+
 interface SocketInterface
 {
+    public function setSocketOption(SocketOptionInterface $option): void;
+
+    public function getSocketOption(): ?SocketOptionInterface;
+
     public function sendAll(string $data, float $timeout = 0): false|int;
 
     public function recvAll(int $length = 65536, float $timeout = 0): false|string;
